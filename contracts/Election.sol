33c265fd916d2b9842514fe036811a62780eab4c
zinping <haozinping@gmail.com>
2024-05-15 17:23:35 -0700

    complete project without none-complete frontend

---
+++ contracts/Election.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.5.16;
+
+contract Election {
+    // Read/write candidate
+    string public candidate;
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Read/write Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    // Constructor
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    
+}
--- truffle-config.js
+++ truffle-config.js
@@ -5,10 +5,10 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
     },
     develop: {
-      port: 8545
-    }
-  }
+      port: 8545,
+    },
+  },
 };
